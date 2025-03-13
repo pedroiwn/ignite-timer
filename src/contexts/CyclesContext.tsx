93f947f919a1b01189ec9990dae37c9b6e858a5f
Pedro Wim <monicavieira170285@gmail.com>
2025-03-13 16:33:32 -0300

    Ignite timer

---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,108 @@
+import { createContext, ReactNode, useState, useReducer, useEffect } from "react";
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducers';
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
+
+interface CreateCycleData {
+  task: string;
+  minutesAmount: number;
+}
+
+interface CyclesContextType {
+  cycles: Cycle[];
+  activeCycle: Cycle | undefined;
+  activeCycleId: string | null;
+  amountSecondsPassed: number;
+  markCurrentCycleAsFinished: () => void;
+  setSecondsPassed: (seconds: number) => void;
+  createNewCycle: (data: CreateCycleData) => void;
+  interruptCurrentCycle: () => void;
+}
+
+export const CyclesContext = createContext({} as CyclesContextType);
+
+interface CyclesContextProviderProps {
+  children: ReactNode;
+}
+
+export function CyclesContextProvider({ 
+  children,
+}: CyclesContextProviderProps) {
+  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
+      cycles: [],
+      activeCycleId: null,
+    }, 
+    (inicialState) => {
+      const storedStateAsJson = localStorage.getItem(
+        '@inite-timer: cycles-state-1.0.0'
+      )
+
+      if (storedStateAsJson) {
+        return JSON.parse(storedStateAsJson);
+      }
+
+      return inicialState
+    },
+  )
+
+  const { cycles, activeCycleId } = cyclesState;
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(),new Date(activeCycle.startDate))
+    }
+
+    return 0
+  });
+
+  useEffect(() => {
+    const stateJson = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@inite-timer: cycles-state-1.0.0', stateJson)
+  }, [cyclesState])
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds);
+  }
+
+  function markCurrentCycleAsFinished() {
+    dispatch(markCurrentCycleAsFinishedAction());
+  }
+
+  function createNewCycle(data: CreateCycleData) {
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    };
+
+    dispatch(addNewCycleAction(newCycle));
+
+    setAmountSecondsPassed(0);
+  }
+
+  function interruptCurrentCycle() {
+    dispatch(interruptCurrentCycleAction());
+  }
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        cycles,
+        activeCycle,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        amountSecondsPassed,
+        setSecondsPassed,
+        createNewCycle,
+        interruptCurrentCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  );
+}
